04c6751c47c725f3711c96bdb05d2e3d8d7703a3
acruiz86 <alie.castillo86@gmail.com>
2023-04-27 16:00:52 -0400

    Actualizando funciones

--- tarea1.py
+++ tarea1.py
@@ -3,9 +3,3 @@ app = Flask(__name__)
 @app.route('/')
 def holaMundo():
     return 'Mundo feliz!'
-@app.route('/nuevo/')
-def endPoint1():
-    return 'el mundo es mejor!'
-@app.route('/pruaba4/')
-def endPoint2():
-    return 'Ya todo cambio'
