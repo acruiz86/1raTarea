3a5a6418c83bbb66245fe11fc6068d6cca6499e5
acruiz86 <alie.castillo86@gmail.com>
2023-04-27 17:05:54 -0400

    Actualizando fichero

---
+++ run.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def holaMundo():
+    return 'Mundo FELIZ !'
+    
+@app.route('/header/')
+def headerSite():
+    a=8
+    b=50
+    c=a*b/5 
+    return 'Encabezado de sitio !'
+
+@app.route('/container/')
+def containerSite():
+    return 'contenido del sitio !'
