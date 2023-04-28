acaa146f07f51a0dfda97d4069546680567e9626
acruiz86 <alie.castillo86@gmail.com>
2023-04-27 22:40:15 -0400

    Actualizando funcion Header

--- run.py
+++ run.py
@@ -3,9 +3,14 @@ app = Flask(__name__)
 @app.route('/')
 def holaMundo():
     return 'Mundo FELIZ !'
+    
 @app.route('/header/')
 def headerSite():
+    a=8
+    b=50
+    c=a*b/5 
     return 'Encabezado de sitio !'
+
 @app.route('/container/')
 def containerSite():
     return 'contenido del sitio !'
