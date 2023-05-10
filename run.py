d8a96436634608f7b2c661cd8146cb8519ecf096
acruiz86 <alie.castillo86@gmail.com>
2023-05-10 11:12:52 -0400

    Actualizando run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def holaMundo():
-    return 'Mundo FELIZ !'
+    return 'Mundo FELIZ  Aprendiendo Jenkins!'
 
 @app.route('/header/')
 def headerSite():
